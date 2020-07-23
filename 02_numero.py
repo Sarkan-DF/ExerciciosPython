bdd73dd731709c64d247100d9216021921ebbb4f
Sarkan-DF <63614609+Sarkan-DF@users.noreply.github.com>
2020-07-22 21:22:13 -0300

    Delete 02_numero.py

--- 02_numero.py
+++
@@ -1,4 +0,0 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-numero = int(input("Digite um numero: "))
-print(f"O número informado foi {numero}")
