b828946c2903265d6041d5dc1fe23b484b0e525a
Sarkan-DF <63614609+Sarkan-DF@users.noreply.github.com>
2022-01-31 16:25:04 -0300

    Update 01_zero_dez.py

--- EstruturaDeRepetição/01_zero_dez.py
+++ EstruturaDeRepetição/01_zero_dez.py
@@ -1,10 +1,14 @@
 # Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue
 # pedindo até que o usuário informe um valor válido.
 
-numero = int(input("Digite um numero de 0 a 10: "))
-
-while numero < 0 or numero > 10:
-    print("Numero incorreto!")
-    numero = int(input("Favor digitar um numero de 0 a 10: "))
-
-print("Numero correto parabens!")
+while True:
+  try:
+    numero = int(input("Digite um numero de 0 a 10: "))
+  except ValueError:
+    print("Deve ser fornecido um valor inteiro!")
+  else:
+    if numero >= 0 and numero <=10:
+      print(f"Numero correto: {numero}!")
+      break
+    else:
+      print("O numero tem que ser de 0 a 10!")
