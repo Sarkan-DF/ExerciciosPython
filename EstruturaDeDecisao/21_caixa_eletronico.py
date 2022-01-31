61599d0f544125537821ca60eb76ab9c0dd288ad
Sarkan-DF <63614609+Sarkan-DF@users.noreply.github.com>
2022-01-31 13:57:53 -0300

    Update 21_caixa_eletronico.py

--- EstruturaDeDecisao/21_caixa_eletronico.py
+++ EstruturaDeDecisao/21_caixa_eletronico.py
@@ -7,43 +7,34 @@
 # Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50,
 # quatro notas de 10, uma nota de 5 e quatro notas de 1.
 
-import math
+valor_saque = int(input("Qual valor deseja sacar lembrando que o valor minimo é R$10,00 e o maximo R$600,00: "))
+nota_um = nota_cinco = nota_dez = nota_cinquenta = nota_cem = 0
 
-valor = int(input("Digite o valor a ser sacado entre R$10 e R$600,00: "))
-valor_original = valor
+while(valor_saque < 10.0 or valor_saque >600.00):
+  print("Valor incoreto!")
+  valor_saque = int(input("Qual valor deseja sacar lembrando que o valor minimo é R$10,00 e o maximo R$600,00: "))
 
-if (valor < 10) or (valor > 600):
-    print("Valor Invalido de saque!")
+#descobrindo nota 100
+nota_cem, valor_saque = divmod(valor_saque,100)
 
-else:
-    nota_cem = valor/100
-    nota_cem = math.floor(nota_cem)
-    valor = valor - (nota_cem * 100)
+#descobrindo nota 50
+nota_cinquenta, valor_saque = divmod(valor_saque,50)
 
-    nota_cinquenta = valor/50
-    nota_cinquenta = math.floor(nota_cinquenta)
-    valor = valor - (nota_cinquenta * 50)
+#descobrindo nota 10
+nota_dez, valor_saque = divmod(valor_saque,10)
 
-    nota_dez = valor/10
-    nota_dez = math.floor(nota_dez)
-    valor = valor - (nota_dez * 10)
+#descobrindo nota 5
+nota_cinco, nota_um = divmod(valor_saque,5)
 
-    nota_cinco = valor/5
-    nota_cinco = math.floor(nota_cinco)
-    valor = valor - (nota_cinco * 5)
+print(f"Para o saque no valor de R${valor_original} sairá do caixa::")
 
-    nota_um = valor/1
-    nota_um = math.floor(nota_um)
-    valor = valor - (nota_um * 1)
-
-    print(f"Para o saque no valor de R${valor_original} sairá do caixa::")
-    if nota_cem > 0:
-        print(f"{nota_cem} nota(s) de cem.")
-    if nota_cinquenta > 0:
-        print(f"{nota_cinquenta} nota(s) de cinquenta.")
-    if nota_dez > 0:
-        print(f"{nota_dez} nota(s) de dez.")
-    if nota_cinco > 0:
-        print(f"{nota_cinco} nota(s) de cinco.")
-    if nota_um > 0:
-        print(f"{nota_um} nota(s) de um.")
+if nota_cem > 0:
+  print(f"{nota_cem} nota(s) de cem.")
+if nota_cinquenta > 0:
+  print(f"{nota_cinquenta} nota(s) de cinquenta.")
+if nota_dez > 0:
+  print(f"{nota_dez} nota(s) de dez.")
+if nota_cinco > 0:
+  print(f"{nota_cinco} nota(s) de cinco.")
+if nota_um > 0:
+  print(f"{nota_um} nota(s) de um.")
