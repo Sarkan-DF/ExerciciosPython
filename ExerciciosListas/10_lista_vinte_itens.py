85cc7beb935855b7290fb967df02cea99524ab83
Sarkan-DF <sarkan.igor@gmail.com>
2022-02-24 21:35:58 -0300

    10

--- ExerciciosListas/10_lista_vinte_itens.py
+++ ExerciciosListas/10_lista_vinte_itens.py
@@ -6,17 +6,12 @@ def lista_vinte_itens():
     lista_1 = [1,3,5,7,9,11,13,15,17,19]
     lista_2 = [2,4,6,8,10,12,14,16,18,20]
     lista_3 = []
-    lista_4 = []
     for i, x in zip(lista_1,lista_2):
-        numero = i, x
-        lista_3.append(numero)
+        num_1, num_2 = i, x
+        lista_3.append(num_1)
+        lista_3.append(num_2)
 
-    for i in lista_3:
-        lista_4.append(i[0])
-        lista_4.append(i[1])
-
-
-    return lista_4
+    return lista_3
 
 if __name__ == '__main__':
     print(lista_vinte_itens())
--- ExerciciosListas/11_lista_trinta_itens.py
+++ ExerciciosListas/11_lista_trinta_itens.py
@@ -3,22 +3,17 @@
 """
 
 def lista_vinte_itens():
-    lista_1 = [1,4,7,10]
-    lista_2 = [2,5,8,11]
-    lista_3 = [3,6,9,12]
+    lista_1 = [1,4,7,10,13,16,19,22,25,28]
+    lista_2 = [2,5,8,11,14,17,20,23,26,29]
+    lista_3 = [3,6,9,12,15,18,21,24,27,30]
     lista_4 = []
-    lista_5 = []
     for i, x, y in zip(lista_1, lista_2, lista_3):
-        numero = i, x, y
-        lista_4.append(numero)
+        num_1, num_2, num_3 = i, x, y
+        lista_4.append(num_1)
+        lista_4.append(num_2)
+        lista_4.append(num_3)
 
-    for i in lista_4:
-        lista_5.append(i[0])
-        lista_5.append(i[1])
-        lista_5.append(i[2])
-
-
-    return lista_5
+    return lista_4
 
 if __name__ == '__main__':
     print(lista_vinte_itens())
