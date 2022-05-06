332755a885a3ca6d3eb3c3fa704e2ea8114054ab
Sarkan-DF <sarkan.igor@gmail.com>
2022-05-06 01:23:35 -0300

    37

--- EstruturaDeRepetição/37_altura_peso.py
+++ EstruturaDeRepetição/37_altura_peso.py
@@ -7,40 +7,74 @@ magro, além da média das alturas e dos pesos dos clientes
 """
 
 
-class AlturaPeso:
-    def __init__(self):
-        pass
-
-    def altura_peso(self):
-        codigo = 1
+def altura_peso():
+    codigo = 1
+    lista_dados = []
+    lista_dados2 = []
+    while codigo != 0:
+        codigo = int(input("Qual o seu codigo? "))
+        if codigo == 0:
+            break
+        peso = int(input("Qual o seu peso? "))
+        altura = float(input("Qual a seu altura? "))
+        print()
+        lista_dados.append(codigo)
+        lista_dados.append(peso)
+        lista_dados.append(altura)
+        lista_dados2.append(lista_dados)
         lista_dados = []
-        lista_dados2 = []
-        while codigo != 0:
-            codigo = int(input("Qual o seu codigo? "))
-            if codigo == 0:
-                break
-            peso = int(input("Qual o seu peso? "))
-            altura = float(input("Qual a seu altura? "))
-            lista_dados.append(codigo)
-            lista_dados.append(peso)
-            lista_dados.append(altura)
-            lista_dados2.append(lista_dados)
-            lista_dados = []
 
-        return lista_dados2
+    return lista_dados2
 
-    def mais_pesado(self):
-        lista_dados = altura_peso.altura_peso()
-        lista_dados2 = []
-        for i in lista_dados:
-            lista_dados2.append(i[1])
-        mais_pesado = max(lista_dados2)
 
-        print(lista_dados2)
-        posicao = lista_dados2.index(mais_pesado)
-        codigo_mais_pesado = (lista_dados[posicao][0])
-        print(f"O aluno mais pesado é o codigo {codigo_mais_pesado} e pesa {mais_pesado}Kg.")
+lista_dados = altura_peso()
+
+def mais_alto():
+    lista_dados2 = []
+    for i in lista_dados:
+        lista_dados2.append(i[2])
+    mais_alto = max(lista_dados2)
+
+    posicao = lista_dados2.index(mais_alto)
+    codigo_mais_alto = (lista_dados[posicao][0])
+    print(f"O aluno mais alto é o codigo {codigo_mais_alto} e tem {mais_alto:.2f}m de altura.")
+
+
+def mais_baixo():
+    lista_dados2 = []
+    for i in lista_dados:
+        lista_dados2.append(i[2])
+    mais_baixo = min(lista_dados2)
+
+    posicao = lista_dados2.index(mais_baixo)
+    codigo_mais_baixo = (lista_dados[posicao][0])
+    print(f"O aluno mais alto é o codigo {codigo_mais_baixo} e tem {mais_baixo:.2f}m de altura.")
+
+
+def mais_pesado():
+    lista_dados2 = []
+    for i in lista_dados:
+        lista_dados2.append(i[1])
+    mais_pesado = max(lista_dados2)
+
+    posicao = lista_dados2.index(mais_pesado)
+    codigo_mais_pesado = (lista_dados[posicao][0])
+    print(f"O aluno mais pesado é o codigo {codigo_mais_pesado} e pesa {mais_pesado}Kg.")
+
+
+def mais_leve():
+    lista_dados2 = []
+    for i in lista_dados:
+        lista_dados2.append(i[1])
+    mais_leve = min(lista_dados2)
+
+    posicao = lista_dados2.index(mais_leve)
+    codigo_mais_leve = (lista_dados[posicao][0])
+    print(f"O aluno mais leve é o codigo {codigo_mais_leve} e pesa {mais_leve}Kg.")
+
 
 if __name__ == '__main__':
-    altura_peso = AlturaPeso()
-    altura_peso.mais_pesado()
+    #mais_pesado()
+    #mais_leve()
+    mais_alto()
+    mais_baixo()
